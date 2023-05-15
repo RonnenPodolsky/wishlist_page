c989a99f407fa722787d9d9589993a3e213b3df6
ronnenn podolsky <podolsky.ronnen@gmail.com>
2023-05-15 15:07:47 +0300

    init

---
+++ src/components/FeaturesList.jsx
@@ -0,0 +1,22 @@
+const FeaturesList = () => {
+  return (
+    <div className='flex gap-10'>
+      <ul className=' border-sky-900 p-4 border-4 	'>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  );
+};
+
+export default FeaturesList;
